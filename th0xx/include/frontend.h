31afd4dd993c7cc4e19adf962e9bcabdda399a21
jsong1b <141759463+jsong1b@users.noreply.github.com>
2025-04-15 13:24:37 -0700

    Setup basic project structure with CMake

---
+++ th0xx/include/frontend.h
@@ -0,0 +1,6 @@
+#ifndef FRONTEND_H
+#define FRONTEND_H
+
+void selectKeeb(void);
+
+#endif // FRONTEND_H
---
+++ th0xx/src/select_keeb.cc
@@ -0,0 +1,10 @@
+// This is a function that prompts the user to select a keyboard. Returns
+// either a pointer to the keyboard or NULL if the user decides to exit the
+// the program or when there is an error.
+
+#include "../include/frontend.h"
+
+void selectKeeb(void) {
+    // TODO(shell): implement `select_keeb`
+    return;
+}
